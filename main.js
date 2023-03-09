251e4f59a6064a3adf053ffef516d67653684bc2
JuanDavidAvilaRaveloCampus <juanavila8856@gmail.com>
2023-03-09 09:04:29 -0500

    feat: :memo: Math.max() and Math.min()
    Descripción con sus respectivos ejemplos de los operadores Math.max() y Math.min()

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 // OPERADORES ANS
 let tabla = {
     "NOMBRE" : "DESCRIPCIÓN",
-    "Math.PI" : "Representa la relación entre la longitud de la circunferencia de un circulo y su diametro, la cual es aproximadamente 3.14159"
+    "Math.max(x1, x2...)" : "Retorna el mayor número de cero o más números dados como parámetros de entrada, o NaN si quialquier parámetro no es un número y no puede ser convertido en uno.",
+    "Math.min(x1, x2...)" : "Retorna el menor número de cero o más números dados como parámetros de entrada, o NaN si quialquier parámetro no es un número y no puede ser convertido en uno."
 }
 console.table(tabla)
 
 // EJEMPLO
-let ejemplo = 2 * Math.PI
-console.log("Este es un ejemplo\n" + ejemplo)
+console.log("Ejemplo con Math.max()\nHallando el número mayor de 1-10\n\nEste número es: " + Math.max(1,2,3,4,5,6,7,8,9,10))
 
 // EJEMPLO 2
-let ejemplo2 = prompt("Ingresa un número cualquiera")
-let entero = Number(ejemplo2) * Math.PI
-console.log(entero)
+console.log("Ejemplo con Math.min()\nHallando el número mayor de 1-10\n\nEste número es: " + Math.min(1,2,3,4,5,6,7,8,9,10))
