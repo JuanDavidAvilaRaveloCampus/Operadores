9f78d36dcc9152ffd14563550dc99a75476c404b
JuanDavidAvilaRaveloCampus <juanavila8856@gmail.com>
2023-03-09 08:37:59 -0500

    feat: :rocket: Operador Math.PI
    Descripción y ejemplos del operador

---
+++ main.js
@@ -0,0 +1,15 @@
+// OPERADORES ANS
+let tabla = {
+    "NOMBRE" : "DESCRIPCIÓN",
+    "Math.PI" : "Representa la relación entre la longitud de la circunferencia de un circulo y su diametro, la cual es aproximadamente 3.14159"
+}
+console.table(tabla)
+
+// EJEMPLO
+let ejemplo = 2 * Math.PI
+console.log("Este es un ejemplo\n" + ejemplo)
+
+// EJEMPLO 2
+let ejemplo2 = prompt("Ingresa un número cualquiera")
+let entero = Number(ejemplo2) * Math.PI
+console.log(entero)
