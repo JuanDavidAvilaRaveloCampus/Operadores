405edaf34d794b21c11fcf3ac8cede4ce468afa5
JuanDavidAvilaRaveloCampus <juanavila8856@gmail.com>
2023-03-09 09:10:38 -0500

    feat: :memo: Math.pow()
    Descripción y ejemplos del operador Math.pow()

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 // OPERADORES ANS
 let tabla = {
     "NOMBRE" : "DESCRIPCIÓN",
-    "Math.max(x1, x2...)" : "Retorna el mayor número de cero o más números dados como parámetros de entrada, o NaN si quialquier parámetro no es un número y no puede ser convertido en uno.",
-    "Math.min(x1, x2...)" : "Retorna el menor número de cero o más números dados como parámetros de entrada, o NaN si quialquier parámetro no es un número y no puede ser convertido en uno."
-}
-console.table(tabla)
+    "Math.pow(base(base,exp)" : "Retorna la base elevada al exponente "
+};
+console.table(tabla);
 
 // EJEMPLO
-console.log("Ejemplo con Math.max()\nHallando el número mayor de 1-10\n\nEste número es: " + Math.max(1,2,3,4,5,6,7,8,9,10))
+console.log("Ejemplo con Math.pow()...\nHallando el número mayor de 1-10\nEste número es: " + Math.pow(2,2));
 
 // EJEMPLO 2
-console.log("Ejemplo con Math.min()\nHallando el número mayor de 1-10\n\nEste número es: " + Math.min(1,2,3,4,5,6,7,8,9,10))
+console.log("Ejemplo 2 con Math.pow()...\nHallando el número mayor de 1-10\nEste número es: " + Math.pow(2,8));
