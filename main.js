f6f22a802980a7c402bf2e5d10ac6556f73384f3
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-03-09 20:08:45 -0500

    feat: :memo: Math.trunc()
    Descripción y ejemplos

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // OPERADORES ANS
 let tabla = {
     "NOMBRE" : "DESCRIPCIÓN",
-    "Math.round(x)" : "Redondea un número al entero más cercano. Si es <= .49 será redondeado hacia abajo y si es >= .5 será redondeado hacia arriba."
+    "Math.trunc(x)" : "Remueve digitos decimales"
 };
 console.table(tabla);
 
 // EJEMPLO
-console.log("Ejemplo con Math.round()...Redondeando un número hacia abajo. Siendo N = 1.49, entonces Math.round(N) = " + Math.round(1.49));
+console.log("Ejemplo con Math.trunc()...Removiendo los decimales de N. Siendo N = 1.49, entonces Math.trunc(N) = " + Math.trunc(1.49));
 
 // EJEMPLO 2
-console.log("Ejemplo 2 con Math.round()...Redondeando un número hacia arriba. Siendo N = 5.5, entonces Math.round(N) = " + Math.round(5.5));
+console.log("Ejemplo 2 con Math.trunc()...Removiendo los decimales de N. Siendo N = 5.5856, entonces Math.trunc(N) = " + Math.trunc(5.5856));
